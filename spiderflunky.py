d29fbfb2e152a656006f4dd5f1655a92c74d2b2f
Erik Rose <erik@mozilla.com>
2013-02-06 13:50:26 -0800

    Kill unused ContextGenerator. Rename get_tree to "parse". Make exception normal.

--- spiderflunky.py
+++ spiderflunky.py
@@ -13,20 +13,18 @@ JS_ESCAPE = re.compile("\\\\+[ux]", re.I)
 SPIDERMONKEY_INSTALLATION = '/usr/local/bin/js'
 
 
-class JSReflectException(Exception):
-    """An exception to indicate that tokenization has failed."""
+class JsReflectException(Exception):
+    """Raised when something goes wrong with parsing using Reflect.parse"""
 
-    def __init__(self, value):
+    def __init__(self, value, line=None):
         self.value = value
-        self.line = None
+        self.line = line
+
+    def __unicode__(self):
+        return 'Line %i: %s' % (self.line, self.value)
 
     def __str__(self):
-        return repr(self.value)
-
-    def line_num(self, line_num):
-        "Set the line number and return self for chaining"
-        self.line = int(line_num)
-        return self
+        return self.__unicode__().encode('utf-8')
 
 
 def prepare_code(code):
@@ -37,7 +35,7 @@ def prepare_code(code):
     return JS_ESCAPE.sub("u", code)
 
 
-def get_tree(code, shell=SPIDERMONKEY_INSTALLATION):
+def parse(code, shell=SPIDERMONKEY_INSTALLATION):
     """Return an AST tree of the JS passed in `code`."""
 
     if not code:
@@ -81,7 +79,7 @@ def get_tree(code, shell=SPIDERMONKEY_INSTALLATION):
             pass
 
     if not data:
-        raise JSReflectException("Reflection failed")
+        raise JsReflectException("Reflection failed")
 
     data = decode(data)
     parsed = json.loads(data, strict=False)
@@ -93,8 +91,8 @@ def get_tree(code, shell=SPIDERMONKEY_INSTALLATION):
                                "spidermonkey='%s'" % (parsed["error_message"],
                                                       shell))
         else:
-            raise JSReflectException(parsed["error_message"]).line_num(
-                    parsed["line_number"])
+            raise JsReflectException(parsed["error_message"],
+                                     line=parsed["line_number"])
 
     return parsed
 
@@ -162,121 +160,5 @@ def filter_ascii(text):
     return "".join((x if is_standard_ascii(x) else "?") for x in text)
 
 
-
-INFINITY = float("inf")
-
-
-# From https://github.com/mattbasta/app-validator/blob/e474775bfce7ef2ce55b3e1bd3bcacf74f587a30/appvalidator/contextgenerator.py
-class ContextGenerator:
-    """The context generator creates a line-by-line mapping of all files that
-    are validated. It will then use that to help produce useful bits of code
-    for errors, warnings, and the like."""
-
-    def __init__(self, data=None):
-        self.data = data.split("\n")
-
-    def get_context(self, line=1, column=0):
-        """Return a tuple containing the context for a line."""
-
-        line -= 1  # The line is one-based
-
-        # If there is no data in the file, there can be no context.
-        datalen = len(self.data)
-        if datalen <= line:
-            return None
-
-        build = [self.data[line]]
-
-        # Add surrounding lines if they're available. There must always be
-        # three elements in the context.
-        if line > 0:
-            build.insert(0, self.data[line - 1])
-        else:
-            build.insert(0, None)
-
-        if line < datalen - 1:
-            build.append(self.data[line + 1])
-        else:
-            build.append(None)
-
-        leading_counts = []
-
-        # Count whitespace to determine how much needs to be stripped.
-        lstrip_count = INFINITY
-        for line in build:
-            # Don't count empty/whitespace-only lines.
-            if line is None or not line.strip():
-                leading_counts.append(lstrip_count)
-                continue
-
-            # Isolate the leading whitespace.
-            ws_count = len(line) - len(line.lstrip())
-            leading_counts.append(ws_count)
-            if ws_count < lstrip_count:
-                lstrip_count = ws_count
-
-        # If all of the lines were skipped over, it means everything was
-        # whitespace.
-        if lstrip_count == INFINITY:
-            return ("", "", "")
-
-        for lnum in range(3):
-            # Skip edge lines.
-            if not build[lnum]:
-                continue
-
-            line = build[lnum].strip()
-
-            # Empty lines stay empty.
-            if not line:
-                build[lnum] = ""
-                continue
-
-            line = self._format_line(line, column=column, rel_line=lnum)
-            line = "%s%s" % (" " * (leading_counts[lnum] - lstrip_count), line)
-
-            build[lnum] = line
-
-        # Return the final output as a tuple.
-        return tuple(build)
-
-    def _format_line(self, data, column=0, rel_line=1):
-        """Formats a line from the data to be the appropriate length."""
-        line_length = len(data)
-
-        if line_length > 140:
-            if rel_line == 0:
-                # Trim from the beginning
-                data = "... %s" % data[-140:]
-            elif rel_line == 1:
-                # Trim surrounding the error position
-                if column < 70:
-                    data = "%s ..." % data[:140]
-                elif column > line_length - 70:
-                    data = "... %s" % data[-140:]
-                else:
-                    data = "... %s ..." % data[column - 70:column + 70]
-
-            elif rel_line == 2:
-                # Trim from the end
-                data = "%s ..." % data[:140]
-
-        return decode(data)
-
-    def get_line(self, position):
-        """Returns the line number that the given string position is found on."""
-
-        datalen = len(self.data)
-        count = len(self.data[0])
-        line = 1
-        while count < position:
-            if line >= datalen:
-                break
-            count += len(self.data[line]) + 1
-            line += 1
-
-        return line
-
-
 if __name__ == '__main__':
-    print get_tree('function frob() {return 8;}')
+    print parse('function frob() {return 8;}')
