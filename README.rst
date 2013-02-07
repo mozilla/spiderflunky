1fec8be5ad8f66c7910dadfbe0ee5749339febdc
Erik Rose <erik@mozilla.com>
2013-02-07 14:24:33 -0800

    Write a readme, make a setup.py, and turn it into a package.
    This will ultimately probably be copied and pasted into the DXR repo, but I guess we'll be a package until then. Or maybe it'll be an excuse to fix up DXR's hook points for external plugins.

---
+++ README.rst
@@ -0,0 +1,27 @@
+============
+spiderflunky
+============
+
+Spiderflunky is a proof-of-concept JS static analysis tool based on Mozilla's
+SpiderMonkey runtime. It uses SpiderMonkey's ``Reflect.parse`` to do the
+parsing and then takes it from there with a flattened-out form of alias
+analysis. Ultimately, it will develop into a JS plugin for DXR
+(http://dxr.allizom.org/).
+
+
+Getting Started
+===============
+
+Build SpiderMonkey's ``js`` executable. Check out mozilla-central, and then
+build the interpreter::
+
+    cd mozilla-central
+    cd js/src
+    autoconf213  # must be 2.13
+    ./configure
+    make
+    sudo cp dist/bin/js /usr/local/bin/
+
+All you can really do so far is run the tests. Enjoy doing that! ::
+
+    python setup.py test
---
+++ setup.py
@@ -0,0 +1,36 @@
+import sys
+
+# Prevent spurious errors during `python setup.py test`, a la
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='spiderflunky',
+    version='0.1',
+    description='Static analysis for JavaScript',
+    long_description=open('README.rst').read(),
+    author='Erik Rose',
+    author_email='erik@mozilla.com',
+    license='MPL',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=['more_itertools>=2.1'],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    url='https://github.com/erikrose/spiderflunky',
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+        ],
+    keywords=['static analysis', 'javascript', 'js', 'parser', 'alias analysis']
+)
