5237e9d20d4164be0472a46144cfd056b2cd3e2c
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-08-07 09:02:48 -0700

    write scaffolding for processing nodes

--- spiderflunky/indexer.py
+++ spiderflunky/indexer.py
@@ -1,10 +1,11 @@
-from funcy import group_by, constantly
+from funcy import group_by, constantly, walk, identity
 
 
 FUNC_GROUP = constantly('function')
 VAR_GROUP = constantly('variable')
 CALL_GROUP = constantly('call')
 SYM_GROUP = constantly('symbol')
+NONE_GROUP = constantly('None')
 
 GROUPS = {
     'Function': FUNC_GROUP,
@@ -18,9 +19,21 @@ GROUPS = {
 }
 
 
-def categorize(node):
-    return GROUPS.get(node.get('type'))(node)
+def _categorize(node):
+    return GROUPS.get(node.get('type'), NONE_GROUP)(node)
+
+
+def categorize(ast):
+    return group_by(_categorize, ast.walk_down())
+
+
+PROCESS = {
+}
+
+
+def process((group, vals)):
+    return map(PROCESS.get(group, identity), vals)
 
 
 def transform(ast):
-    return group_by(categorize, ast.walk_down())
+    return walk(process, categorize(ast))
